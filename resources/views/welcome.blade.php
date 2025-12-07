40b3ea8dd608343e12fcf8e2e58b50a289de4a14
Gauro11 <milbertgaringa5@.com>
2025-12-07 01:54:23 -0800

    as

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/jpeg" href="mg1.jpg">
+    <link rel="icon" type="image/jpeg" href="icon-192.png">
     <link rel="manifest" href="manifest.json">
     <meta name="theme-color" content="#22d3ee">
     <title>Milbert Garinga</title>
